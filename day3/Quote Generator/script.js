b5b35486465171c7a361a62521ac6cf2cc65fc41
= <=>
2021-04-16 17:52:13 +0200

    enhance perfomance of the api request

--- day3/Quote Generator/script.js
+++ day3/Quote Generator/script.js
@@ -3,14 +3,6 @@ const quoteText  = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
-
-//Show Loading 
-function loading(){
-    loader.hidden = false;
-    quoteContainer.hidden = true;
-    loader.hidden = true; 
-}
 
 
 //random quote 
@@ -18,41 +10,25 @@ function randomQuote(max){
     let num = Math.random();
     return Math.floor(num* max); 
 }
-// Hide Loading 
-function complete (){
-    loading();
-    if (!loader.hidden){
-        quoteContainer.hidden = false ;
-    }
-}
+
 //Get Quote From Api 
-async function getQuote(){
-    
-    const apiUrl = 'https://type.fit/api/quotes';
+const apiUrl = 'https://type.fit/api/quotes';
+let quotes = [];
 
-    try{
-        const response = await fetch( apiUrl );
-        const quotes = await response.json();
-        const index = randomQuote(quotes.length);
-        const quote = quotes[index]; 
+async function boot (){
+    const response = await fetch(apiUrl);
+    quotes  = await response.json();
+    getQuote();
+}
 
-        // display quote 
-        authorText.innerText = quote.author; 
-        
-      // reduce font size for long quotes
-      if(quote.text.length >120){
-          quoteText.classList.add('long-quote');
-      } else {
-          quoteText.classList.remove('long-quote');
-      }
-     quoteText.innerText = quote.text;
-     //Stope Loader, Show Quote 
-     complete();
-    } catch(error){
-        console.error(error);
-    }
+function getQuote(){
+    const index = randomQuote(quotes.length);
+    const quote = quotes[index];
+    authorText.innerText = quote.author; 
+    quoteText.innerText = quote.text;
 
 }
+
 //Tweet Quote
 function tweetQuote(){
     const quote = quoteText.innerText;
@@ -61,9 +37,10 @@ function tweetQuote(){
     window.open(twitterUrl,'_blank');
     
 }
-//Event Listeners
 
+//Event Listeners
 newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click',tweetQuote);
-//On load 
-getQuote();
+
+//fetch data from api and display random quote 
+boot();
